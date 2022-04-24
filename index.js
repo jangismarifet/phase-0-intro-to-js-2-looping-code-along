4ee23344464ee00a4a2f3d858ecaffa300f9e79d
jangismarifet <jangismarifet@gmail.com>
2022-04-24 23:05:05 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arr,name) {
+    let newArr = [];
+    for (let i =0;i < arr.length;i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${name} gift!`);
+    }
+    return newArr;
+}
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
